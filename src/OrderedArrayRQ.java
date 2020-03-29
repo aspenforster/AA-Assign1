c8b3c171ce8b3e3a70e90026f316cd7ff0999b99
aspenforster <aspenforster@gmail.com>
2020-03-29 21:16:41 +1100

    adding ordered array enqueue + dequeue

--- src/OrderedArrayRQ.java
+++ src/OrderedArrayRQ.java
@@ -35,35 +35,110 @@ public class OrderedArrayRQ implements Runqueue {
     }
 
     private Proc[] procArray;
+    private int procLength = 0;
     /**
      * Constructs empty queue
      */
     public OrderedArrayRQ() {
         // Implement Me
-        procArray = new Proc[1];
+        procArray = new Proc[6];
+        procArray[0] = new Proc("t0", 1000);
+        procArray[1] = new Proc("t1", 700);
+        procArray[2] = new Proc("t2", 200);
+
+        procArray[3] = new Proc("t3", 100);
+        procArray[4] = new Proc("t4", 50);
+        procArray[5] = new Proc("t5", 20);
+        procLength = 6;
         System.out.println(procArray);
     }  // end of OrderedArrayRQ()
 
 
+    //returns index to put new value at
+    public int binarySearchIndex(Proc[] array, int leftIndex, int rightIndex, int value){
+
+        //midpoint is the left index plus half the difference between left and right
+        if (rightIndex >= leftIndex){
+            
+            int mid = leftIndex + (rightIndex - leftIndex)/2;
+            System.out.println("checking index " + mid + " in range " + leftIndex + " " + rightIndex);
+            if (value == array[mid].virtualRuntime){
+                System.out.println("found at " + mid);
+                return mid;
+            } else if (value > array[mid].virtualRuntime){
+                System.out.println("searching left half");
+                return binarySearchIndex(array, leftIndex, mid -1, value);
+            } else if (value < array[mid].virtualRuntime){
+                System.out.println("searching right half");
+                return binarySearchIndex(array, mid + 1, rightIndex, value);
+            }
+        }
+        System.out.println("not found" + leftIndex + " " + rightIndex);
+        //leftIndex at this point is the index that the new value should be put at
+        //even if they're the same value (this will push a proc with the same runtime but longer on queue closer to being dequeued)
+        return leftIndex;
+    }
+
     @Override
     public void enqueue(String procLabel, int vt) {
-        // Implement me
+        //array empty
+        if (procLength == 0)
+        {
+            procArray[0] = new Proc(procLabel, vt);
+            procLength++;    
+        }
+        
+        else 
+        {
+            Proc[] temp = new Proc[procLength + 1];
+
+            //index at which enqueue'd proc should be placed
+            int procIndex = binarySearchIndex(procArray, 0, procLength -1, vt);
+
+            //copy across earlier values that weren't affected by enqueue
+            System.arraycopy(procArray, 0, temp, 0, procIndex);
+            //put new proc at index
+            temp[procIndex] = new Proc(procLabel, vt);
+            //added new proc so increase actual length by 1
+            procLength++;
+            //copy across remaining values from original array at (original index) + 1
+            System.arraycopy(procArray, procIndex, temp, procIndex + 1, procLength - 1 - procIndex);
+
+            System.out.print("procArray = ");
+            for(int i =0; i < procLength -1; i++){
+                System.out.print(procArray[i].getVirtualRuntime() + " ");
+            }
+            System.out.println();
+
+            System.out.print("temp = ");
+            for(int i=0; i < procLength; i++){
+                System.out.print(temp[i].getVirtualRuntime() + " ");
+            }
+            System.out.println();
+
+            //replace old array with new one
+            procArray = temp;
+        }
 
     } // end of enqueue()
 
 
     @Override
     public String dequeue() {
-        // Implement me
-
-        return ""; // placeholder,modify this
+        if (procLength > 0){
+            String procLabel = procArray[procLength- 1].getLabel();
+            procArray[procLength - 1] = null;
+            procLength--;
+            return procLabel;
+        }
+        return ""; 
     } // end of dequeue()
 
 
     @Override
     public boolean findProcess(String procLabel){
         // Implement me
-
+       
         return false; // placeholder, modify this
     } // end of findProcess()
 
@@ -94,8 +169,10 @@ public class OrderedArrayRQ implements Runqueue {
 
     @Override
     public void printAllProcesses(PrintWriter os) {
-        //Implement me
-
+        for (int i = procLength -1; i >= 0; i--){
+            System.out.println(procArray[i].getLabel() + "-" + procArray[i].getVirtualRuntime());
+        }
+        
     } // end of printAllProcesses()
 
 } // end of class OrderedArrayRQ
